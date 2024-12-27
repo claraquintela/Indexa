86a8e23e2944d34344355b4b332100bc36381de6
Clara Quintela <claraquintela@gmail.com>
2024-12-27 14:27:20 -0500

    Add filter funcionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { ContainerComponent } from "./components/container/container.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { SeparadorComponent } from "./components/separador/separador.component";
 import { ContatoComponent } from './components/contato/contato.component';
+import {FormsModule} from '@angular/forms' ;
 
 interface Contato {
   id: number
@@ -17,11 +18,31 @@ import agenda from './agenda.json';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent, SeparadorComponent,ContatoComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent, SeparadorComponent,ContatoComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  alphabet: string = 'abcdefghijklmnopqrstuvwxyz'
-  contacts: Contato[]
+  alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
+  contacts: Contato[] = agenda;
+  textFilter: string =''
+
+  //to remove a string's accents
+  private removeAccents(text: string) : string {
+    return text.normalize("NFD".replace(/[\u0300-\u036f]/g, ''));
+  }
+
+  filterContactsByText (): Contato[] {
+    if(!this.textFilter){
+      return this.contacts
+    }
+    return this.contacts.filter(contact => {
+      return this.removeAccents(contact.nome).toLowerCase().includes(this.removeAccents(this.textFilter).toLowerCase())
+    })
+  }
+
+  filterContactsByLetter (letter:string) : Contato[] {
+    return this.filterContactsByText().filter(contact => { return this.removeAccents(contact.nome).toLowerCase().startsWith(letter)})
+  }
 }
