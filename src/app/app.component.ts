0568068ee6c8ff76ae18a4f25a9825b481729125
Clara Quintela <claraquintela@gmail.com>
2024-12-27 11:28:09 -0500

    Adding contacts from json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./components/container/container.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { SeparadorComponent } from "./components/separador/separador.component";
+import { ContatoComponent } from './components/contato/contato.component';
+
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
+
+import agenda from './agenda.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent, SeparadorComponent,ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alphabet: string = 'abcdefghijklmnopqrstuvwxyz'
+  contacts: Contato[]
 }
